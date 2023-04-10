50d7dce36d3163c11400d9d108c6bc6ec6e573aa
Gruzin <gruzd.roman@inbox.ru>
2023-04-10 15:03:21 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,12 +1,7 @@
-import inspect
 import time
-
-
-
 
 def log(fn):
     def log_wrapper(*args, **kwargs):
-
         params = fn.__code__.co_varnames[:fn.__code__.co_argcount]
         params_list = list(params)
         args_list = list(args)
@@ -23,30 +18,27 @@ def log(fn):
         st = time.time()
         result = fn(*args, **kwargs)
         run_time = time.time() - st
-
         with open("log.txt", "w", encoding="utf-8") as log_file:
-            log_file.write(f"{fn.__name__};")
+            log_file.write(f"{fn.__name__}; ")
             args_field = ""
             for key, val in args_dict.items():
-                args_field += f"{key}={val},"
+                args_field += f" {key}={val},"
             args_field = args_field[:-1]
             args_field += ";"
             log_file.write("args:")
             log_file.write(args_field)
             kwargs_field = ""
             for key, val in kwargs.items():
-                kwargs_field += f"{key}={val},"
+                kwargs_field += f" {key}={val},"
             kwargs_field = kwargs_field[:-1]
             kwargs_field += ";"
-            log_file.write("kwargs:")
+            log_file.write(" kwargs:")
             log_file.write(kwargs_field)
-            log_file.write(f"execution time: {run_time} sec.")
-
+            log_file.write(f" execution time: {run_time} sec.")
     return log_wrapper
 
 @log
-def foo(a, b, c, d):
+def foo(a, b, c):
     return 0
 
-if __name__ == "__main__":
-    foo(1, 2, c=3, d=4)
+foo(1, 2, c=3)
